5b4cd9b7039874e488304624e6338ad0d3f445b4
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2019-03-25 22:48:30 -0500

    Created Block Matching stereopsis script

---
+++ StereopsisBM.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+imgL = cv2.imread('./calibPictures/Stonehenge1.png', 0)
+imgR = cv2.imread('./calibPictures/Stonehenge2.png', 0)
+
+stereoMatcher = cv2.StereoBM_create()
+stereoMatcher.setMinDisparity(45)
+stereoMatcher.setBlockSize(15)
+disparity = stereoMatcher.compute(imgL, imgR)
+plt.imshow(disparity,'gray')
+plt.show()
