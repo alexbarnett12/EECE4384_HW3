3c9a7a1a5aa1f514c1a9714c4ecfb6534108e0c1
Alexander Barnett <alexander.barnett@vanderbilt.edu>
2019-04-07 14:47:53 -0500

    Fixed errors

--- StereopsisBM.py
+++ StereopsisBM.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-# imgL = cv2.imread('./calibPictures/Stonehenge1.png', 0)
-# imgR = cv2.imread('./calibPictures/Stonehenge2.png', 0)
+
 imgL = cv2.imread('classroom1.png', 0)
 imgR = cv2.imread('classroom2.png', 0)
-# gray_left = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-# gray_right = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+
+imgLG = cv2.GaussianBlur(imgL,(11,11),0)
+imgRG = cv2.GaussianBlur(imgR,(11,11),0)
 
 stereoMatcher = cv2.StereoBM_create(256, 25)
 
@@ -15,5 +15,9 @@ stereoMatcher = cv2.StereoBM_create(256, 25)
 # stereoMatcher.setMinDisparity(256)
 # stereoMatcher.setBlockSize(25)
 disparity = stereoMatcher.compute(imgL, imgR)
+disparityG = stereoMatcher.compute(imgLG, imgRG)
 plt.imshow(disparity,'gray')
 plt.show()
+
+cv2.imwrite('./images/BM_disp.png',disparity)
+cv2.imwrite('./images/BM_disp_GB.png',disparityG)
